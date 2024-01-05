2cd24a371b30e4ff24bb71736e43615a9bf91f76
Supraja Ramaraju <test.email@example.com>
2024-01-05 17:10:54 +0000

    Enrolment and Login model classes

--- StudentEnrolmentWebApp/Models/Enrollment.cs
+++ StudentEnrolmentWebApp/Models/Enrollment.cs
@@ -2,5 +2,11 @@
 {
     public class Enrollment
     {
+        public int EnrolmentNumber { get; set; }
+        public int StudentID { get; set;}
+        public int CourseID { get; set; }
+        public string GPA { get; set;}
+
+
     }
 }
